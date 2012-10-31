c442c5be055c0b8751bf20c062e783eaa9edcd86
Skri Patch <sp@blky.eu>
2012-10-31 12:21:41 +0100

    logerrorNPEhotfix

--- 2-8080.cmd
+++ 2-8080.cmd
@@ -1,1 +1,1 @@
-mvn -o -e  help:active-profiles tomcat:redeploy -Dcccache -DTOMCAT6 -DLOCAL8080 -Dmaven.test.skip=true
+mvn -o -X  help:active-profiles tomcat:redeploy -Dcccache -DTOMCAT6 -DLOCAL8080 -Dmaven.test.skip=true
